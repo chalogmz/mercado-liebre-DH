db36c98c852995f4ee34c9552d701ee8a781532e
Salome Gomez <sgomez713@misena.edu.co>
2021-10-22 20:58:27 -0500

    se solucionaron algunos errores en el servidor

--- src/app.js
+++ src/app.js
@@ -6,19 +6,19 @@ const port = process.env.PORT || 7000;
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './view/home.html'));
 });
 
-// app.post('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './view/home.html'));
-// });
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './view/home.html'));
+});
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './view/register.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './view/login.html'));
 });
 
 app.listen(port, () => console.log('Server listening on port'));
